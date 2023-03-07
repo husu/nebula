c1c7e5867a1a7fb0916b54c88728e0cf8bc6ec4b
GitHub Action <action@github.com>
2023-03-07 07:23:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/husu/nebula/MyBundle.kt
+++ src/main/kotlin/com/github/husu/nebula/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.husu.nebula
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/husu/nebula/services/MyProjectService.kt
+++ src/main/kotlin/com/github/husu/nebula/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.husu.nebula.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.husu.nebula.MyBundle
 
 class MyProjectService(project: Project) {
 
